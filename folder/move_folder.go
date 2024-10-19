20c9f64fd8f0a130bb2e5ad92f7d135b29de2f35
Jesse Purcell <jesse.purcell@my.edu.au>
2024-10-19 21:58:04 +1000

    Implement MoveFolder method

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,7 +1,55 @@
 package folder
 
-func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+import (
+	"errors"
+	"strings"
+)
 
-	return []Folder{}, nil
+func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
+
+	folders := GetAllFolders()
+
+	var sourceFolder, destinationFolder *Folder
+	for i := range folders {
+		if folders[i].Name == name {
+			sourceFolder = &folders[i]
+		}
+		if folders[i].Name == dst {
+			destinationFolder = &folders[i]
+		}
+	}
+
+	if sourceFolder == nil {
+		return nil, errors.New("source folder does not exist")
+	}
+	if destinationFolder == nil {
+		return nil, errors.New("destination folder does not exist")
+	}
+	if sourceFolder == destinationFolder {
+		return nil, errors.New("cannot move a folder to itself")
+	}
+	if sourceFolder.OrgId != destinationFolder.OrgId {
+		return nil, errors.New("cannot move a folder to a different organization")
+	}
+	if strings.Contains(destinationFolder.Paths, sourceFolder.Name) {
+		return nil, errors.New("cannot move a folder to a child of itself")
+	}
+
+	var newFolders []Folder
+
+	for _, folder := range folders {
+		if strings.HasPrefix(folder.Paths, sourceFolder.Paths) {
+			relativePath := strings.TrimPrefix(folder.Paths, sourceFolder.Paths)
+			newPath := destinationFolder.Paths + "." + sourceFolder.Name + relativePath
+			movedFolder := Folder{
+				Name:  folder.Name,
+				Paths: newPath,
+				OrgId: folder.OrgId,
+			}
+			newFolders = append(newFolders, movedFolder)
+		} else {
+			newFolders = append(newFolders, folder)
+		}
+	}
+	return newFolders, nil
 }

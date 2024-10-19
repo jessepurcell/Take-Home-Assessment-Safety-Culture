3e60e91139cd199c511a3c22d9303b44fae77910
Jesse Purcell <jesse.purcell@my.edu.au>
2024-10-19 14:52:26 +1000

    Implemented initial GetAllChildFolders test case

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -1,15 +1,19 @@
 package folder_test
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
-	// "github.com/stretchr/testify/assert"
 )
 
 // feel free to change how the unit test is structured
 func Test_folder_GetFoldersByOrgID(t *testing.T) {
+	// Setup test parameters
+	orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
+	allFolders := folder.GetAllFolders()
+
 	t.Parallel()
 	tests := [...]struct {
 		name    string
@@ -17,13 +21,14 @@ func Test_folder_GetFoldersByOrgID(t *testing.T) {
 		folders []folder.Folder
 		want    []folder.Folder
 	}{
-		// TODO: your tests here
+		{"creative", orgID, allFolders, []folder.Folder(nil)},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// f := folder.NewDriver(tt.folders)
-			// get := f.GetFoldersByOrgID(tt.orgID)
-
+			f := folder.NewDriver(tt.folders)
+			//orgFolder := f.GetFoldersByOrgID(tt.orgID)
+			subFolders := f.GetAllChildFolders(tt.orgID, tt.name)
+			assert.Equal(t, tt.want, subFolders)
 		})
 	}
 }
